80b66902c9ecfd46a6da5821a1fccf7d64e44fe3
josephmv43 <josephmv43@gmail.com>
2024-03-28 15:05:18 +0530

    login page and signup page

--- routes/user.js
+++ routes/user.js
@@ -8,6 +8,13 @@ router.get('/', function(req, res, next) {
     res.render('user/index',{products})
   })
 });
+router.get('/login',((req, res, next) => {
+  res.render('user/login')
+}))
 
+router.get('/signUp',(req, res, next) => {
+  console.log('signup')
+  res.render('user/signUp')
+})
 
 module.exports = router;
