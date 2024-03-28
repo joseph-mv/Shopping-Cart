1b89af20b099421dd2c9894669562c37124582e0
josephmv43 <josephmv43@gmail.com>
2024-03-28 12:41:17 +0530

    index.hbs modified and show products by using productHelper.productList

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var productHelper=require('../Helpers/product-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user/index', { title: 'Express' });
+  productHelper.productList().then((products)=>{
+    res.render('user/index',{products})
+  })
 });
 
 
